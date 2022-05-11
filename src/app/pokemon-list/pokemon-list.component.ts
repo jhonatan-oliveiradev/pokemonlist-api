5abbbdfa538ad2d91ad42344a84c3b97bcb8a9cf
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-05-10 22:30:49 -0300

    Add files via upload

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass'],
+})
+export class PokemonListComponent {
+  constructor(public pokemonService: PokemonService) {}
+}
---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PokemonService {
+  pokemons: any[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.carregarPokemons();
+  }
+
+  async carregarPokemons() {
+    const requisicao = await this.httpClient
+      .get<any>('https://pokeapi.co/api/v2/pokemon?limit=900')
+      .toPromise();
+
+    this.pokemons = requisicao.results;
+  }
+}
